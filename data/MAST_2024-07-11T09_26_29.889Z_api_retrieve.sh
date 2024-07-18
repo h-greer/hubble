bebb38349b66d16f3bb67fdd51b19b00e027143b
Hayden Greer <s4748097@student.uq.edu.au>
2024-07-18 19:34:38 +1000

    add the other things that the job will need

---
+++ data/MAST_2024-07-11T09_26_29.889Z_api_retrieve.sh
@@ -0,0 +1,90 @@
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C05030%2Fn43c05030_asn.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C05030/n43c05030_asn.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C05030%2Fn43c05030_asc.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C05030/n43c05030_asc.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C05030%2Fn43c05030_mos.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C05030/n43c05030_mos.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C05030%2Fi7k16544n_flt.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C05030/i7k16544n_flt.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43CA5010%2Fn43ca5010_asn.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43CA5010/n43ca5010_asn.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43CA5010%2Fn43ca5010_asc.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43CA5010/n43ca5010_asc.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43CA5010%2Fn43ca5010_mos.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43CA5010/n43ca5010_mos.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43CA5020%2Fn43ca5020_asn.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43CA5020/n43ca5020_asn.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43CA5020%2Fn43ca5020_asc.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43CA5020/n43ca5020_asc.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43CA5020%2Fn43ca5020_mos.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43CA5020/n43ca5020_mos.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43CA5020%2Fi7k1653rn_flt.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43CA5020/i7k1653rn_flt.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C01010%2Fn43c01010_asn.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C01010/n43c01010_asn.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C01010%2Fn43c01010_asc.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C01010/n43c01010_asc.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C01010%2Fn43c01010_mos.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C01010/n43c01010_mos.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C01020%2Fn43c01020_asn.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C01020/n43c01020_asn.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C01020%2Fn43c01020_asc.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C01020/n43c01020_asc.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C01020%2Fn43c01020_mos.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C01020/n43c01020_mos.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C02010%2Fn43c02010_asn.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C02010/n43c02010_asn.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C02010%2Fn43c02010_asc.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C02010/n43c02010_asc.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C02010%2Fn43c02010_mos.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C02010/n43c02010_mos.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C02010%2Fi7k1653tn_flt.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C02010/i7k1653tn_flt.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C02020%2Fn43c02020_asn.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C02020/n43c02020_asn.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C02020%2Fn43c02020_asc.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C02020/n43c02020_asc.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C02020%2Fn43c02020_mos.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C02020/n43c02020_mos.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C02020%2Fi1f13588n_flt.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C02020/i1f13588n_flt.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C02030%2Fn43c02030_asn.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C02030/n43c02030_asn.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C02030%2Fn43c02030_asc.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C02030/n43c02030_asc.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C02030%2Fn43c02030_mos.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C02030/n43c02030_mos.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C02030%2Fi7k16543n_flt.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C02030/i7k16543n_flt.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C03010%2Fn43c03010_asn.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C03010/n43c03010_asn.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C03010%2Fn43c03010_asc.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C03010/n43c03010_asc.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C03010%2Fn43c03010_mos.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C03010/n43c03010_mos.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C03020%2Fn43c03020_asn.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C03020/n43c03020_asn.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C03020%2Fn43c03020_asc.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C03020/n43c03020_asc.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C03020%2Fn43c03020_mos.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C03020/n43c03020_mos.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C04010%2Fn43c04010_asn.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C04010/n43c04010_asn.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C04010%2Fn43c04010_asc.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C04010/n43c04010_asc.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C04010%2Fn43c04010_mos.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C04010/n43c04010_mos.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C05010%2Fn43c05010_asn.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C05010/n43c05010_asn.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C05010%2Fn43c05010_asc.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C05010/n43c05010_asc.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C05010%2Fn43c05010_mos.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C05010/n43c05010_mos.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C05020%2Fn43c05020_asn.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C05020/n43c05020_asn.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C05020%2Fn43c05020_asc.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C05020/n43c05020_asc.fits" --fail --create-dirs
+
+curl -L -X GET "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product?product_name=N43C05020%2Fn43c05020_mos.fits" --output "MAST_2024-07-11T09_26_05.575Z/HST/N43C05020/n43c05020_mos.fits" --fail --create-dirs
+
+curl -H "Authorization: token $MAST_API_TOKEN" -L -X POST "https://mast.stsci.edu/search/hst/api/v0.1/retrieve_product_zip" -F "mission=HST" -F "structure=nested" -F uri="N43C05030/n43c05030_asn.fits,N43C05030/n43c05030_asc.fits,N43C05030/n43c05030_mos.fits,N43C05030/i7k16544n_flt.fits,N43CA5010/n43ca5010_asn.fits,N43CA5010/n43ca5010_asc.fits,N43CA5010/n43ca5010_mos.fits,N43CA5020/n43ca5020_asn.fits,N43CA5020/n43ca5020_asc.fits,N43CA5020/n43ca5020_mos.fits,N43CA5020/i7k1653rn_flt.fits,N43C01010/n43c01010_asn.fits,N43C01010/n43c01010_asc.fits,N43C01010/n43c01010_mos.fits,N43C01020/n43c01020_asn.fits,N43C01020/n43c01020_asc.fits,N43C01020/n43c01020_mos.fits,N43C02010/n43c02010_asn.fits,N43C02010/n43c02010_asc.fits,N43C02010/n43c02010_mos.fits,N43C02010/i7k1653tn_flt.fits,N43C02020/n43c02020_asn.fits,N43C02020/n43c02020_asc.fits,N43C02020/n43c02020_mos.fits,N43C02020/i1f13588n_flt.fits,N43C02030/n43c02030_asn.fits,N43C02030/n43c02030_asc.fits,N43C02030/n43c02030_mos.fits,N43C02030/i7k16543n_flt.fits,N43C03010/n43c03010_asn.fits,N43C03010/n43c03010_asc.fits,N43C03010/n43c03010_mos.fits,N43C03020/n43c03020_asn.fits,N43C03020/n43c03020_asc.fits,N43C03020/n43c03020_mos.fits,N43C04010/n43c04010_asn.fits,N43C04010/n43c04010_asc.fits,N43C04010/n43c04010_mos.fits,N43C05010/n43c05010_asn.fits,N43C05010/n43c05010_asc.fits,N43C05010/n43c05010_mos.fits,N43C05020/n43c05020_asn.fits,N43C05020/n43c05020_asc.fits,N43C05020/n43c05020_mos.fits" -F "manifestonly=true" --output "MAST_2024-07-11T09_26_05.575Z/MANIFEST.html" --fail --create-dirs
+
