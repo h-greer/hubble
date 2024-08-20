4ad9bb55c56b406a9c82d0b35776d703186cd200
Hayden Greer <h.greer@student.uq.edu.au>
2024-08-21 06:48:21 +1000

    give jobs more ram

--- batch/binary_grid.sh
+++ batch/binary_grid.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name binary_grid
 #SBATCH --array=1-20                    
 #SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=4G
+#SBATCH --mem-per-cpu=6G
 #SBATCH --time=1-0:00
 #SBATCH -o out/binary.%a.out  # STDOUT
 #SBATCH -e out/binary.%a.err  # STDERR
