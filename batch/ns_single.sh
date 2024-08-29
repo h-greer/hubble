cc9e948cdede0fb3084d1000888ae3dc42051ebb
Hayden Greer <h.greer@student.uq.edu.au>
2024-08-29 20:14:57 +1000

    more ram for nested sampling

--- batch/ns_single.sh
+++ batch/ns_single.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name ns
 #SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=4G
+#SBATCH --mem-per-cpu=8G
 #SBATCH --time=1-0:00
 #SBATCH -o slurm.%N.%j.out  # STDOUT
 #SBATCH -e slurm.%N.%j.err  # STDERR
