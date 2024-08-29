74e560383a48ef380044695836255e3f094e3aed
Hayden Greer <h.greer@student.uq.edu.au>
2024-08-29 17:14:16 +1000

    nested sampling test to run on cluster

---
+++ batch/ns_single.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name ns
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=1-0:00
+#SBATCH -o slurm.%N.%j.out  # STDOUT
+#SBATCH -e slurm.%N.%j.err  # STDERR
+
+module load anaconda3/5.2.0
+source /opt/modules/Anaconda3/5.2.0/etc/profile.d/conda.sh
+conda activate /data/uqhgreer/repos/jax-cpu
+
+python hmc_single.py
+
+conda deactivate
