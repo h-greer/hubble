f8c2000b4ef6e97f462fd5becf993a1bc1716344
Hayden Greer <h.greer@student.uq.edu.au>
2025-02-19 11:16:08 +1000

    grid search cluster job

---
+++ batch/turbo-grid-search.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name turbo_grid_search
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=1-0:00
+#SBATCH -o turbo.%j.out  # STDOUT
+#SBATCH -e turbo.%j.err  # STDERR
+
+module load anaconda3/5.2.0
+source /opt/modules/Anaconda3/5.2.0/etc/profile.d/conda.sh
+conda activate /data/uqhgreer/repos/jax-cpu
+
+srun python turbo-grid-search.py
+
+conda deactivate
