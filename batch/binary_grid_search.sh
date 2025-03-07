aa879c3b9dd93665225f5acd1be59f91e49df1b7
Hayden Greer <h.greer@student.uq.edu.au>
2025-03-08 07:41:37 +1000

    more cores

--- batch/binary_grid_search.sh
+++ batch/binary_grid_search.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name binary_grid_search
 #SBATCH --array=0-67                    
-#SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=4G
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=3G
 #SBATCH --time=6:00:00
 #SBATCH -o binary/%a.out  # STDOUT
 #SBATCH -e binary/%a.err  # STDERR
