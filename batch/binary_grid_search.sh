9ae3bc4a0285b443e36078a8b09d1a2db9158fdb
Hayden Greer <h.greer@student.uq.edu.au>
2025-03-07 14:07:14 +1000

    change job parameters

--- batch/binary_grid_search.sh
+++ batch/binary_grid_search.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name dwarf_analyse
+#SBATCH --job-name binary_grid_search
 #SBATCH --array=0-67                    
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=4G
 #SBATCH --time=6:00
-#SBATCH -o grid/%a.out  # STDOUT
-#SBATCH -e grid/%a.err  # STDERR
+#SBATCH -o binary/%a.out  # STDOUT
+#SBATCH -e binary/%a.err  # STDERR
 
 module load anaconda3/5.2.0
 source /opt/modules/Anaconda3/5.2.0/etc/profile.d/conda.sh
