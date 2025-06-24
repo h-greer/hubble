891491791f1a7d07b9adcb4ff98c188493ba22e1
Hayden Greer <h.greer@student.uq.edu.au>
2025-06-24 18:44:53 +1000

    fix download script

--- batch/download-friday.sh
+++ batch/download-friday.sh
@@ -4,10 +4,17 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=2G
 #SBATCH --job-name=hd189733-download
-#SBATCH --time=1:00:00
+#SBATCH --time=10:00:00
 #SBATCH --partition=general
 #SBATCH -o download-%j.output
 #SBATCH -e download-%j.error
 
+cd ../data
 
-srun bash ../data/hd189733-all.sh
+srun bash hd189733-all.sh
+
+# have to unpack recursive folders
+cd MAST_2025-06-24T0210
+
+find ./HST -mindepth 2 -type f -exec mv {} ./HST/ \;
+find ./HST -mindepth 1 -type d -empty -delete
