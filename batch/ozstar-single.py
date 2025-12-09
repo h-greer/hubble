62237d320d8d82b4b401afd6b3e6a6d1495bdfa1
Hayden Greer <h.greer@student.uq.edu.au>
2025-12-09 17:22:14 +1000

    run on gpu

---
+++ batch/ozstar-single.py
@@ -0,0 +1,402 @@
+# %%
+import sys
+sys.path.insert(0, '..')
+
+# %%
+import optimistix as optx
+
+
+# %%
+# Basic imports
+import jax.numpy as np
+import jax.random as jr
+import jax.scipy as jsp
+import jax
+import numpy
+
+jax.config.update("jax_enable_x64", True)
+
+
+# Optimisation imports
+import zodiax as zdx
+import optax
+
+# dLux imports
+import dLux as dl
+import dLux.utils as dlu
+
+# Visualisation imports
+from tqdm.auto import tqdm
+import matplotlib.pyplot as plt
+import matplotlib
+
+plt.rcParams['image.cmap'] = 'inferno'
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["image.origin"] = 'lower'
+plt.rcParams['figure.dpi'] = 72
+plt.rcParams["font.size"] = 24
+
+from detectors import *
+from apertures import *
+from models import *
+from fisher import *
+from stats import posterior
+from fitting import *
+from plotting import *
+from spectra import *
+
+import jax.tree_util as jtu
+
+def set_array(pytree):
+    dtype = np.float64 if jax.config.x64_enabled else np.float32
+    floats, other = eqx.partition(pytree, eqx.is_inexact_array_like)
+    floats = jtu.tree_map(lambda x: np.array(x, dtype=dtype), floats)
+    return eqx.combine(floats, other)
+
+# %%
+extra_bad = None
+#extra_bad = np.isnan(np.zeros((64, 64)).at[35,60].set(np.nan))
+
+#extra_bad = np.isnan(np.zeros((wid,wid))).at[wid//2-3:wid//2+3,:].set(np.nan)
+
+
+# %%
+wid = 64
+oversample = 4
+
+nwavels = 50#6
+npoly=20#2
+
+n_zernikes = 30#12
+
+optics = NICMOSOptics(1024, wid, oversample, n_zernikes = n_zernikes)
+
+detector = NICMOSDetector(oversample, wid)
+
+ddir = "../data/MAST_2024-09-22T03_37_01.724Z/HST/"
+
+# brown dwarf
+
+files = [
+    #'n8yj53vfq_cal.fits',
+    #'n8yj53vkq_cal.fits',
+    'n8yj02x0q_cal.fits',
+    #'n8yj02wyq_cal.fits'
+
+]
+"""
+
+# lhs 1846
+ddir = "../data/MAST_2025-03-02T22_29_29.259Z/HST/"
+
+files = ["n8ku01ffq_cal.fits", "n8ku01fgq_cal.fits"][:1]
+
+ddir = "../data/MAST_2024-09-08T07_59_18.213Z/HST/"
+
+files = [
+    # F095N
+    "n43ca5fjq_cal.fits",
+    "n43ca5fkq_cal.fits",
+    "n43ca5flq_cal.fits",
+    "n43ca5fmq_cal.fits",
+    #F190N
+    "n43ca5feq_cal.fits",
+    "n43ca5ffq_cal.fits",
+    "n43ca5fgq_cal.fits",
+    "n43ca5fhq_cal.fits",
+    #F187N
+    #"n43c03dnq_cal.fits",
+    #"n43c03doq_cal.fits",
+    #"n43c03dpq_cal.fits",
+    #"n43c03drq_cal.fits",
+    #F108N
+    #"n43c03dsq_cal.fits",
+    #"n43c03duq_cal.fits",
+    ##"n43c03dvq_cal.fits",
+    ##"n43c03dwq_cal.fits",
+][-1:]
+
+# hyades
+
+ddir = "../data/MAST_2024-09-26T22_53_13.719Z/HST/"
+
+files = [
+    "n8ry37p8q_cal.fits",
+    "n8ry37p9q_cal.fits",
+
+    #"n8ry13mqq_cal.fits",
+    #"n8ry13mrq_cal.fits"
+][:-1]
+
+
+ddir = '../data/MAST_2025-03-06T11_35_52.340Z/HST/'
+
+files = [
+    #F110M
+    'n4be02npq_cal.fits',
+    'n4be02nsq_cal.fits',
+    'n4be02nvq_cal.fits',
+    'n4be02nyq_cal.fits',
+
+    #F145M
+    'n4be02nqq_cal.fits',
+    'n4be02ntq_cal.fits',
+    'n4be02nwq_cal.fits',
+    'n4be02o0q_cal.fits',
+]#[:1]#[-2:-1]
+"""
+
+#extra_bad = np.isnan(np.zeros((wid,wid))).at[wid//2-3:wid//2+3,:].set(np.nan)
+
+#exposures_single = [exposure_from_file(ddir + file, SinglePointPolySpectrumFit(nwavels), crop=wid, extra_bad=extra_bad) for file in files]
+
+exposures_single = [exposure_from_file(ddir + file, SinglePointFit(CombinedFourierSpectrum, nwavels), crop=wid, extra_bad=extra_bad) for file in files]
+
+#exposures_binary = [exposure_from_file(ddir + file, BinaryPolySpectrumFit(nwavels), crop=wid, extra_bad=extra_bad) for file in files]
+
+# %%
+for e in exposures_single:
+    print(e.mjd)#*86400)
+    print(e.target)
+    print(e.filter)
+
+# %%
+
+
+# %%
+params = {
+    #"fluxes": {},
+    "positions": {},
+    "spectrum": {},
+    "aberrations": {},
+
+    #"rot": 0.,
+
+    "cold_mask_shift": {},
+    "cold_mask_rot": {},
+    "cold_mask_scale": {},
+    "cold_mask_shear": {},
+    "primary_scale": {},
+    "primary_rot": {},
+    "primary_shear": {},
+    "outer_radius": 1.2*0.955,
+    "secondary_radius": 0.372*1.2,
+    "spider_width": 0.077*1.2,
+    "scale": 0.043142,
+
+    "softening": 2.,#0.1,
+    "bias": {},
+    "jitter": {},
+    #"displacement": 1.#1e5#{}
+}
+
+for exp in exposures_single:
+    params["positions"][exp.fit.get_key(exp, "positions")] = np.asarray([ 0.45184505, -0.8391668 ])#np.asarray([-0.2,0.4])
+    params["spectrum"][exp.fit.get_key(exp, "spectrum")] = np.zeros(npoly).at[0].set(1)*np.log10(np.nansum(exp.data)/nwavels)#np.ones(npoly)*np.log10(np.nansum(exp.data)/nwavels)#(np.zeros(npoly)).at[0].set(1)*np.log10(np.nansum(exp.data)/nwavels)
+    params["aberrations"][exp.fit.get_key(exp, "aberrations")] = np.zeros(n_zernikes)#np.asarray([0., 24.884588  , -25.489779  , -17.15699   , -21.790146  ,
+    #      -4.592212  ,  -4.832893  ,  19.196083  ,   0.37983412,
+    #       7.0756216 ,   0.30277824,  -6.330534])#np.zeros(n_zernikes)
+    params["cold_mask_shift"][exp.fit.get_key(exp, "cold_mask_shift")] = np.asarray([8.,8.])#np.asarray([9.599048, 6.196583])
+    params["cold_mask_rot"][exp.fit.get_key(exp, "cold_mask_rot")] = -45.
+    params["cold_mask_scale"][exp.fit.get_key(exp, "cold_mask_scale")] = np.asarray([1.,1.])
+    params["cold_mask_shear"][exp.fit.get_key(exp, "cold_mask_shear")] = np.asarray([0.,0.])
+    params["primary_rot"][exp.fit.get_key(exp, "primary_rot")] = -45. + 90. 
+    params["primary_scale"][exp.fit.get_key(exp, "primary_scale")] = np.asarray([1.,1.])
+    params["primary_shear"][exp.fit.get_key(exp, "primary_shear")] = np.asarray([0.,0.])
+    #params["displacement"][exp.fit.get_key(exp, "displacement")] = 1e6
+
+    params["bias"][exp.fit.get_key(exp, "bias")] = 0.
+    params["jitter"][exp.fit.get_key(exp, "jitter")] = 7/43*oversample
+
+
+model_single = set_array(NICMOSModel(exposures_single, params, optics, detector))
+#model_binary = set_array(NICMOSModel(exposures_binary, params, optics, detector))
+
+
+params = ModelParams(params)
+
+# %%
+#plot_comparison(model_single, params, exposures_single)
+
+# %%
+plt.imshow(exposures_single[0].err)
+plt.colorbar()
+
+# %%
+cmap = matplotlib.colormaps['inferno']
+cmap.set_bad('k',1)
+plt.figure(figsize=(10,10))
+plt.imshow(exposures_single[0].data**0.125, cmap=cmap)
+plt.title(exposures_single[0].target)
+plt.colorbar()
+
+
+# %%
+def scheduler(lr, start, *args):
+    shed_dict = {start: 1e10}
+    for start, mul in args:
+        shed_dict[start] = mul
+    return optax.piecewise_constant_schedule(lr / 1e10, shed_dict)
+
+base_sgd = lambda vals: optax.sgd(vals, nesterov=True, momentum=0.6)
+
+opt = lambda lr, start, *schedule: base_sgd(scheduler(lr, start, *schedule))
+
+base_sgd2 = lambda vals: optax.rmsprop(vals)
+
+opts = lambda lr, start, *schedule: base_sgd2(scheduler(lr, start, *schedule))
+
+
+base_adam = lambda vals: optax.adam(vals)
+opta = lambda lr, start, *schedule: base_adam(scheduler(lr, start, *schedule))
+
+base_lbfgs = lambda vals: optax.lbfgs(vals)
+
+optl = lambda lr, start, *schedule: base_lbfgs(scheduler(lr, start, *schedule))
+
+
+
+
+def flatten(l):
+    if isinstance(l, (tuple, list)):
+         return [a for i in l for a in flatten(i)]
+    else:
+        return [l]
+
+
+
+g = 5e-2
+
+things = {
+    "positions": opt(g*2, 0),
+    "spectrum": opt(g*5, 10),#opt(g*2, 10),#, (20, 1.5)),
+    "cold_mask_shift": opt(g*30, 30),
+    #"cold_mask_rot": opt(g*10, 100),
+    "bias": opt(g*2, 20),
+    "aberrations": opt(g*1, 50),
+
+    #"cold_mask_scale": opt(g*1, 300),
+    #"cold_mask_shear": opt(g*1, 300),
+    #"primary_scale": opt(g*1, 300),
+    #"primary_shear": opt(g*1, 300),
+}
+
+
+groups = list(things.keys())
+
+# %%
+losses, models = optimise(params, model_single, exposures_single, things, 100, recalculate=False)
+
+# %%
+
+# %%
+print(losses[0], losses[-1])
+
+# %%
+models_pd = [jax.tree.map(lambda x,y: (x-y)/y, models[i], models[-1]) for i in range(len(models))]
+
+print(models[-1].params)
+
+# %%
+models[-1].inject(model_single)
+
+# %%
+models[-1]
+
+# %%
+groups
+
+# %%
+fsh = calc_fishers(models[-1].inject(model_single), exposures_single, groups, fisher_fn, recalculate=True, save=False)
+
+
+# %%
+def loss_fn(params, exposures, model):
+    mdl = params.inject(model)
+    res = np.sum(np.asarray([posterior(mdl,exposure) for exposure in exposures]))
+    return np.where(res==0.0, np.inf, res)
+
+@eqx.filter_jit
+def fun(params, args):
+    exposures, model = args
+    return loss_fn(params, exposures, model)
+
+def optimise_optimistix(params, model, exposures, things, niter):
+    paths = list(things.keys())
+    optimisers = [things[i] for i in paths]
+
+    model_params = ModelParams({p: params.get(p) for p in things.keys()})
+
+    solver = optx.BFGS(rtol=1e-8, atol=1e-6,verbose=frozenset({"step_size", "loss"}))
+    sol = optx.minimise(fun, solver, model_params, (exposures, model), throw=False, max_steps=niter)
+    
+    return sol
+
+
+
+# %%
+sol = optimise_optimistix(models[-1], models[-1].inject(model_single), exposures_single, things, 5000)
+print(sol.value.params)
+print(fun(sol.value, (exposures_single, model_single)), (losses[-1]))
+
+# %%
+final_params = sol.value
+
+# %%
+sol.stats
+
+# %%
+#plot_comparison(final_params.inject(models[-1].inject(model_single)), sol.value, exposures_single)
+
+# %%
+final_params = sol.value
+
+# %%
+fsh = calc_fishers(final_params.inject(model_single), exposures_single, ["spectrum"], fisher_fn, recalculate=True, save=False)
+fsh
+
+# %%
+spectrum_cov = np.linalg.inv(fsh['n8yj02x0q.spectrum'])#+fsh['n8yj02wyq.spectrum'])
+spectrum_err = np.diag(np.sqrt(np.abs(spectrum_cov)))
+
+# %%
+#plt.imshow(spectrum_cov, cmap='seismic', vmin=-np.max(np.abs(spectrum_cov)), vmax=np.max(np.abs(spectrum_cov)))
+#plt.colorbar()
+
+print(spectrum_cov)
+
+
+# %%
+#plt.imshow(np.sign(spectrum_cov))
+
+# %%
+vals, vects = np.linalg.eig(fsh['n8yj02x0q.spectrum'])#+fsh['n8yj02wyq.spectrum'])
+
+order = np.argsort(vals)[::-1]
+
+#plt.figure(figsize=(10,10))
+#plt.xlabel("Coefficient")
+
+for i in range(5):
+    plt.plot(np.arange(npoly),np.real(vects[:,order[i]]), label=f"{i}")
+#plt.legend()
+
+# %%
+#plt.semilogy(np.sort(np.real(vals))[::-1])
+
+# %%
+vals[order]
+
+# %%
+#plt.figure(figsize=(10,10))
+
+wv, filt = calc_throughput("F110W", nwavels=60)
+
+spec = CombinedFourierSpectrum(wv, filt, final_params.get("spectrum.U11296_F110W"))
+
+#HD201592_F110M
+
+#plt.plot(wavels, params.get("spectrum.U10764_F110W"))
+#plt.plot(wv*1e6, spec.spec_weights()*spec.flux)#, yerr = spectrum_err)
+#plt.xlabel("Wavelength (um)")
+
---
+++ batch/ozstar-single.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=single-star
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=0:20:00
+#SBATCH --mem-per-cpu=2000M
+#SBATCH --gres=gpu:1
+
+module load gcccore/13.2.0
+module load python/3.11.5
+
+source /fred/oz440/hayden/venv/bin/activate
+
+python ozstar-single.py
+
+deactivate
