574701ff28d208f77fba5ba9cc874002325f8551
Hayden Greer <h.greer@student.uq.edu.au>
2026-02-11 10:02:37 +1000

    test blackjax on ozstar

---
+++ batch/blackjax-test.py
@@ -0,0 +1,349 @@
+# %%
+import sys
+sys.path.insert(0, '..')
+
+# %%
+# Basic imports
+import jax.numpy as np
+import jax.random as jr
+import jax.scipy as jsp
+import jax
+import numpy
+
+#jax.config.update("jax_enable_x64", True)
+
+
+# Optimisation imports
+import zodiax as zdx
+import optax
+
+# dLux imports
+import dLux as dl
+import dLux.utils as dlu
+
+import blackjax
+
+# Visualisation imports
+from tqdm.auto import tqdm
+import matplotlib.pyplot as plt
+import matplotlib
+
+plt.rcParams['image.cmap'] = 'inferno'
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["image.origin"] = 'lower'
+plt.rcParams['figure.dpi'] = 72
+plt.rcParams["font.size"] = 24
+
+from detectors import *
+from apertures import *
+from models import *
+from fisher import *
+from stats import posterior
+from fitting import *
+from plotting import *
+from spectra import *
+
+import jax.tree_util as jtu
+
+def set_array(pytree):
+    dtype = np.float64 if jax.config.x64_enabled else np.float32
+    floats, other = eqx.partition(pytree, eqx.is_inexact_array_like)
+    floats = jtu.tree_map(lambda x: np.array(x, dtype=dtype), floats)
+    return eqx.combine(floats, other)
+
+# %%
+extra_bad = None
+#extra_bad = np.isnan(np.zeros((64, 64)).at[35,60].set(np.nan))
+
+#extra_bad = np.isnan(np.zeros((wid,wid))).at[wid//2-3:wid//2+3,:].set(np.nan)
+
+
+# %%
+wid = 64
+oversample = 4
+
+nwavels = 3
+npoly=1
+
+n_zernikes = 12#12
+
+optics = NICMOSOptics(1024, wid, oversample, n_zernikes = n_zernikes)
+
+detector = NICMOSDetector(oversample, wid)
+
+ddir = "../data/MAST_2024-09-22T03_37_01.724Z/HST/"
+
+spectrum_basis = np.ones((nwavels, npoly))
+
+
+ddir = "../data/MAST_2026-02-02T23_18_00.739Z/HST/"
+
+files_f190N = """
+n8o101a1q_cal.fits
+n8o101a2q_cal.fits
+n8o101a3q_cal.fits
+""".split()[:1]
+
+
+files_f108N = """
+n8o102j5q_cal.fits
+n8o101adq_cal.fits
+n8o101atq_cal.fits
+n8o101b9q_cal.fits
+""".split()[:1]
+
+exposures_f108N = [exposure_from_file(ddir + file, BinaryFit(spectrum_basis, "F108N"), crop=wid) for file in files_f108N]
+exposures_f190N = [exposure_from_file(ddir + file, BinaryFit(spectrum_basis, "F190N"), crop=wid) for file in files_f190N]
+exposures_single = exposures_f190N#exposures_f108N+exposures_f190N
+
+# %%
+for e in exposures_single:
+    print(e.mjd)#*86400)
+    print(e.target)
+    print(e.filter)
+
+# %%
+
+
+# %%
+params = {
+    "positions": {},
+    "primary_spectrum": {},
+    "secondary_spectrum": {},
+
+    "aberrations": {},
+
+    "separation": 2.,
+    "position_angle": 110.,
+
+
+    "cold_mask_shift": {},
+    "cold_mask_rot": {},
+    "cold_mask_scale": {},
+    "cold_mask_shear": {},
+    "primary_scale": {},
+    "primary_rot": {},
+    "primary_shear": {},
+    "outer_radius": 1.2*0.955,
+    "secondary_radius": 0.372*1.2,
+    "spider_width": 0.077*1.2,
+    "scale": 0.043142,
+
+    "softening": 2.,#0.1,
+    "bias": {},
+    "jitter": {},
+    #"displacement": 1.#1e5#{}
+}
+
+
+for idx, exp in enumerate(exposures_single):
+    params["positions"][exp.fit.get_key(exp, "positions")] = np.asarray([0.,0.])
+    params["primary_spectrum"][exp.fit.get_key(exp, "primary_spectrum")] = (np.zeros(npoly)).at[0].set(np.log10(np.nansum(exp.data)/nwavels))#np.array([3.2]) if exp.filter == "F108N" else np.array([3.5]) 
+    params["secondary_spectrum"][exp.fit.get_key(exp, "secondary_spectrum")] = (np.zeros(npoly)).at[0].set(np.log10(np.nansum(exp.data)/nwavels)) - np.log10(6.5)
+
+    params["aberrations"][exp.fit.get_key(exp, "aberrations")] = np.zeros(n_zernikes)
+
+    params["cold_mask_shift"][exp.fit.get_key(exp, "cold_mask_shift")] = np.asarray([8.,8.])
+    params["cold_mask_rot"][exp.fit.get_key(exp, "cold_mask_rot")] = -45.
+    params["cold_mask_scale"][exp.fit.get_key(exp, "cold_mask_scale")] = np.asarray([1.,1.])
+    params["cold_mask_shear"][exp.fit.get_key(exp, "cold_mask_shear")] = np.asarray([0.,0.])
+    params["primary_rot"][exp.fit.get_key(exp, "primary_rot")] = -45. + 90. 
+    params["primary_scale"][exp.fit.get_key(exp, "primary_scale")] = np.asarray([1.,1.])
+    params["primary_shear"][exp.fit.get_key(exp, "primary_shear")] = np.asarray([0.,0.])
+    #params["displacement"][exp.fit.get_key(exp, "displacement")] = 1e6
+
+    params["bias"][exp.fit.get_key(exp, "bias")] = 0.
+    params["jitter"][exp.fit.get_key(exp, "jitter")] = 7/43*oversample
+
+
+model_single = set_array(NICMOSModel(exposures_single, params, optics, detector))
+#model_binary = set_array(NICMOSModel(exposures_binary, params, optics, detector))
+
+
+params = ModelParams(params)
+
+# %%
+print(np.nansum(exp.data))
+
+# %%
+np.log10(7336/np.sum(10**spectrum_basis[:,0]))
+
+# %%
+print(params.params)
+
+# %%
+plot_comparison(model_single, params, exposures_single)
+
+# %%
+plt.imshow(exposures_single[0].err)
+plt.colorbar()
+
+# %%
+plt.imshow(np.log10(exposures_single[0].data/exposures_single[0].err))
+plt.colorbar()
+
+# %%
+print(exposures_single[0].exptime)
+
+# %%
+plt.imshow(exposures_single[0].data**0.125)
+plt.xticks([])
+plt.yticks([])
+
+# %%
+cmap = matplotlib.colormaps['inferno']
+cmap.set_bad('k',1)
+plt.figure(figsize=(10,10))
+plt.imshow(exposures_single[0].data**0.125, cmap=cmap)
+plt.title(exposures_single[0].target)
+plt.colorbar()
+
+
+# %%
+def scheduler(lr, start, *args):
+    shed_dict = {start: 1e10}
+    for start, mul in args:
+        shed_dict[start] = mul
+    return optax.piecewise_constant_schedule(lr / 1e10, shed_dict)
+
+base_sgd = lambda vals: optax.sgd(vals, nesterov=True, momentum=0.6)
+
+opt = lambda lr, start, *schedule: base_sgd(scheduler(lr, start, *schedule))
+
+base_sgd2 = lambda vals: optax.noisy_sgd(vals)
+
+opts = lambda lr, start, *schedule: base_sgd2(scheduler(lr, start, *schedule))
+
+
+base_adam = lambda vals: optax.adam(vals)
+opta = lambda lr, start, *schedule: base_adam(scheduler(lr, start, *schedule))
+
+
+
+
+
+def flatten(l):
+    if isinstance(l, (tuple, list)):
+         return [a for i in l for a in flatten(i)]
+    else:
+        return [l]
+
+
+
+g = 5e-2
+
+things = {
+    "positions": opt(g*5, 0),
+    "position_angle": opt(g*10, 10),
+    "separation": opt(g*15, 20),
+    "primary_spectrum": opt(g*5, 30),
+    "secondary_spectrum": opt(g*3, 30),
+    "cold_mask_shift": opt(g*10, 60),
+    #"cold_mask_rot": opt(g*10, 100),
+    "bias": opt(g*5, 50),
+    "aberrations": opt(g*0.05, 80),
+
+    #"cold_mask_scale": opt(g*1, 300),
+    #"cold_mask_shear": opt(g*1, 300),
+    #"primary_scale": opt(g*1, 300),
+    #"primary_shear": opt(g*1, 300),
+}
+
+things_start = {
+    "positions": opt(g*5, 0),
+}
+
+groups = list(things.keys())
+
+# %%
+initial_losses, initial_models = optimise(params, model_single, exposures_single, things_start, 10, recalculate=True)
+
+# %%
+plot_comparison(model_single, initial_models[-1], exposures_single)
+
+# %%
+initial_models[-1].params
+
+# %%
+losses, models = optimise(initial_models[-1].inject(params), initial_models[-1].inject(model_single), exposures_single, things, 300, recalculate=False)
+
+# %%
+plt.plot(np.asarray(losses[-50:])/(len(exposures_single)*wid**2))
+
+# %%
+print(losses[0], losses[-1])
+
+# %%
+models_pd = [jax.tree.map(lambda x,y: (x-y)/y, models[i], models[-1]) for i in range(len(models))]
+
+# %%
+plot_params(models, groups, xw = 3)
+plot_comparison(model_single, models[-1], exposures_single)
+
+# %%
+print(models[-1].params)
+
+# %%
+print(models[-1].params["primary_spectrum"])
+print(models[-1].params["secondary_spectrum"])
+print(models[-1].params["separation"]*42)
+print(models[-1].params["position_angle"])
+
+# %%
+models[-1]
+
+# %%
+def loss_fn(params, exposures, model):
+    mdl = params.inject(model)
+    return np.nansum(np.asarray([posterior(mdl,exposure) for exposure in exposures]))
+
+loglike = lambda params: -loss_fn(params, exposures_single, models[-1].inject(model_single))
+
+
+# %%
+def inference_loop(rng_key, kernel, initial_state, num_samples):
+    @jax.jit
+    def one_step(state, rng_key):
+        state, _ = kernel(rng_key, state)
+        return state, state
+
+    keys = jax.random.split(rng_key, num_samples)
+    _, states = jax.lax.scan(one_step, initial_state, keys)
+
+    return states
+
+# %%
+models[-1]
+
+# %%
+initial_position = ModelParams({"positions": models[-1].get("positions"), "position_angle": models[-1].get("position_angle")})
+rng_key = jr.key(0)
+
+warmup = blackjax.window_adaptation(blackjax.nuts, loglike, progress_bar=True)
+rng_key, warmup_key, sample_key = jax.random.split(rng_key, 3)
+(state, parameters), _ = warmup.run(warmup_key, initial_position, num_steps=100)
+
+kernel = blackjax.nuts(loglike, **parameters).step
+states = inference_loop(sample_key, kernel, state, 100)
+
+mcmc_samples = states.position
+#mcmc_samples["scale"] = np.exp(mcmc_samples["log_scale"]).block_until_ready()
+
+
+# %%
+mcmc_samples.get("positions.n8o101a1q")[:,0]
+
+# %%
+plt.scatter(mcmc_samples.get("positions.n8o101a1q")[:,0], mcmc_samples.get("positions.n8o101a1q")[:,1])
+
+# %%
+for key in mcmc_samples.keys():
+    val = mcmc_samples[key]
+    if type(val) is not dict:
+        print("yes")
+    print(mcmc_samples[key])
+
+# %%
+mcmc_samples.keys()
+
+
