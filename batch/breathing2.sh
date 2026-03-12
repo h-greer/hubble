f7c2c9d99ad07755bd50ec661f603f7f8e97c98e
Hayden Greer <h.greer@student.uq.edu.au>
2026-03-13 10:53:55 +1100

    full breathing mode run

--- batch/breathing2.sh
+++ batch/breathing2.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=00:15:00
 #SBATCH --mem-per-cpu=5000M
 #SBATCH -o breathing-data/exp%a.out
-#SBATCH --array=0-5
+#SBATCH --array=2048-4380
 
 module load python-scientific/3.13.1-foss-2025a
 
