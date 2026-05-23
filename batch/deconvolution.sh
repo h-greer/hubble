7eb0af9f7b435b17c87e583408b870998c61b7e3
Hayden Greer <h.greer@student.uq.edu.au>
2026-05-24 09:31:23 +1000

    fix venv

--- batch/deconvolution.sh
+++ batch/deconvolution.sh
@@ -3,14 +3,13 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=0:30:00
+#SBATCH --time=1:00:00
 #SBATCH --mem-per-cpu=2000M
 #SBATCH --gres=gpu:1
 
-module load gcccore/13.2.0
-module load python/3.11.5
+module load python-scientific/3.13.1-foss-2025a
 
-source /fred/oz440/hayden/venv/bin/activate
+source /fred/oz440/hayden/new-hubble/.venv/bin/activate
 
 python deconvolution2.py
 
